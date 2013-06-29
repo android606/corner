8347ec7307b79d24d0f69df7078b50554a4b2477
Andrew Mark <android606@gmail.com>
2013-06-29 11:10:10 -0700

    Better comments for ContextProvider class

--- Corner/src/com/diabolicalschema/corner/ContextProvider.java
+++ Corner/src/com/diabolicalschema/corner/ContextProvider.java
@@ -3,6 +3,20 @@ package com.diabolicalschema.corner;
 import android.app.Application;
 import android.content.Context;
 
+/** ContextProvider
+ * 
+ * This is just a handy place to grab the application context so I can grab it when I want
+ * from any other part of the program without having to explicitly pass it everywhere.
+ * 'Cause that's lame.
+ * 
+ * Also, it works with static classes and utility classes that don't inherit from Activity.
+ * 
+ * Anywhere you need context to store settings or whatever, use:
+ * ContextProvider.getContext();
+ * 
+ * @author android
+ *
+ */
 public class ContextProvider extends Application {
 
 	/**
